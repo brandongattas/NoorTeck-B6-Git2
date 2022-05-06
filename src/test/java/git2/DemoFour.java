4c3c8b3ed481b0f03ae0aa0b0829c435474a0ce5
Brandon <Brandon@10.0.0.12>
2022-05-05 21:21:28 -0400

    added new class.

--- src/test/java/git2/DemoFour.java
+++ src/test/java/git2/DemoFour.java
@@ -5,5 +5,10 @@ public class DemoFour {
 	public static void main(String[] args) {
 		System.out.println("Demo Four...");
 	}
+	
+	public static void div(int a, int b) {
+		int result=a/b;
+		System.out.println(result);
+	}
 
 }
